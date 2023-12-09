3db72142cf4a02105aac80dd8bcaecc5e12bb2b3
Monik <moniknarshibhai.nakrani@edu.sait.ca>
2023-12-09 01:26:09 -0500

    added another database connection

--- File_Integrity_Anti_Virus.py
+++ File_Integrity_Anti_Virus.py
@@ -1,43 +1,29 @@
 import os
 import hashlib
-import mysql.connector
+import firebase_admin
+from firebase_admin import credentials, firestore
 from datetime import datetime
 
-# Function to establish a connection to the MySQL database
-def connect_to_database():
+def initialize_firebase(credentials_path):
     try:
-        conn = mysql.connector.connect(
-            host='host',
-            user='user',
-            password='password',
-            database='database'
-        )
-        cursor = conn.cursor()
+        # Initialize Firebase with your credentials
+        cred = credentials.Certificate(credentials_path)
+        firebase_admin.initialize_app(cred)
+        print("Firebase initialized successfully.")
+    except Exception as e:
+        print(f"Error initializing Firebase: {e}")
 
-        # Check if the timestamp column exists
-        cursor.execute("SHOW COLUMNS FROM file_changes LIKE 'timestamp'")
-        result = cursor.fetchone()
+def connect_to_firestore():
+    db = firestore.client()
+    return db
 
-        if not result:
-            # The timestamp column does not exist, so add it
-            cursor.execute("ALTER TABLE file_changes ADD COLUMN timestamp DATETIME")
-            conn.commit()
-
-        return conn, cursor
-
-    except mysql.connector.Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        return None, None
-
-# Function to insert file change information into the database with timestamp
-def insert_alert_to_database(cursor, filename, conn):
+def insert_alert_to_firestore(db, filename):
     try:
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        cursor.execute('INSERT INTO file_changes (filename, timestamp) VALUES (%s, %s)', (filename, timestamp))
-        conn.commit()
-        print(f"Alert: {filename} has been modified. Alert sent to the database at {timestamp}")
-    except mysql.connector.Error as e:
-        print(f"Error inserting alert to the database: {e}")
+        db.collection('file_changes').add({'filename': filename, 'timestamp': timestamp})
+        print(f"Alert: {filename} has been modified. Alert sent to Firestore at {timestamp}")
+    except Exception as e:
+        print(f"Error inserting alert to Firestore: {e}")
 
 def calculate_file_hash(file_path):
     sha256_hash = hashlib.sha256()
@@ -46,7 +32,7 @@ def calculate_file_hash(file_path):
             sha256_hash.update(byte_block)
     return sha256_hash.hexdigest()
 
-def check_file_changes(directory, conn, cursor, file_hashes):
+def check_file_changes(directory, db, file_hashes):
     while True:
         for filename in os.listdir(directory):
             if filename.endswith(".py"):
@@ -55,8 +41,8 @@ def check_file_changes(directory, conn, cursor, file_hashes):
 
                 if file_path in file_hashes:
                     if file_hashes[file_path] != current_hash:
-                        # File has been modified, send alert to the database
-                        insert_alert_to_database(cursor, filename, conn)
+                        # File has been modified, send alert to Firestore
+                        insert_alert_to_firestore(db, filename)
                 else:
                     file_hashes[file_path] = current_hash
 
@@ -65,22 +51,23 @@ def check_file_changes(directory, conn, cursor, file_hashes):
 
         except KeyboardInterrupt:
             print("Exiting the program.")
-            if conn:
-                conn.close()
             break
 
 if __name__ == "__main__":
-    directory_path = "D:\CapstoneGithub\Project-Capstone"
+    # Replace 'path to credentials.json' with the actual path to your Firebase credentials JSON file
+    credentials_path = r''
+    initialize_firebase(credentials_path)
 
-    conn, cursor = connect_to_database()
+    # Connect to Firestore
+    firestore_db = connect_to_firestore()
+
+    directory_path = r""
 
     # Initialize the file_hashes dictionary
     file_hashes = {}
 
     try:
-        check_file_changes(directory_path, conn, cursor, file_hashes)
+        check_file_changes(directory_path, firestore_db, file_hashes)
 
     except KeyboardInterrupt:
         print("Exiting the program.")
-        if conn:
-            conn.close()
